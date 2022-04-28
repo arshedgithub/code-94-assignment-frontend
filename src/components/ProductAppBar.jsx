a8ffafdef5f02f38ce8d1b556c796fa5aec7bd29
Arshed Ahmed <arshedahmedmail@gmail.com>
2022-04-28 12:12:54 +0530

    add product app bar

---
+++ src/components/ProductAppBar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+// material UI component
+import { Button, Toolbar, Typography } from "@mui/material";
+
+const ProductAppBAr = () => {
+  return (
+    <div>
+      <Typography variant="h6" component="div" SX={{ letterSpacing: 2 }}>
+        PRODUCTS
+      </Typography>
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} />
+        <Button variant="contained" size="large" color="primary">
+          New Product
+        </Button>
+        <Button variant="outlined" size="large" color="primary"></Button>
+      </Toolbar>
+    </div>
+  );
+};
+
+export default ProductAppBAr;
