af9552c2088845723d0333777c54747a6b784ea4
Arshed Ahmed <arshedahmedmail@gmail.com>
2022-04-28 16:52:10 +0530

    creating the table for products

--- src/components/ProductAppBar.jsx
+++ src/components/ProductAppBar.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-// material UI component
-import { Button, createTheme, Toolbar, Typography } from "@mui/material";
+// material UI components
+import {
+  Button,
+  createTheme,
+  InputBase,
+  Paper,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { ThemeProvider } from "@emotion/react";
+import SearchIcon from "@mui/icons-material/Search";
+// svg star icon
+import Star from "../assets/svg/star.svg";
+import { Link } from "react-router-dom";
 
 // customizing the theme
 const theme = createTheme({
@@ -15,19 +26,65 @@ const theme = createTheme({
 const ProductAppBar = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Typography
-        variant="h5"
-        component="div"
-        sx={{ letterSpacing: 2, fontWeight: "bold" }}
-      >
-        PRODUCTS
-      </Typography>
       <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} />
-        <Button variant="contained" size="large" color="primary" sx={{ m: 1 }}>
-          New Product
-        </Button>
-        <Button variant="outlined" size="large" color="primary"></Button>
+        <Typography
+          variant="h5"
+          component="div"
+          size="large"
+          sx={{ letterSpacing: 2, fontWeight: "bold" }}
+        >
+          PRODUCTS
+        </Typography>
+      </Toolbar>
+
+      <Toolbar>
+        <Paper
+          component="form"
+          sx={{
+            p: "1px 4px",
+            mr: 20,
+            display: "flex",
+            alignItems: "center",
+            width: 300,
+            boxShadow: "none",
+            backgroundColor: "#F7F7F7",
+            borderRadius: "20px",
+            flexGrow: 1,
+          }}
+        >
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="search for products"
+          />
+          <Button
+            variant="contained"
+            size="small"
+            color="primary"
+            sx={{ m: 1, textDecoration: "none", borderRadius: "18px", px: 2 }}
+          >
+            <SearchIcon />
+            search
+          </Button>
+        </Paper>
+
+        {/* <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} /> */}
+        <Link to="/new-product">
+          <Button
+            variant="contained"
+            size="large"
+            color="primary"
+            sx={{ m: 1, textDecoration: "none" }}
+          >
+            New Product
+          </Button>
+        </Link>
+
+        {/* star icon */}
+        <Link to="/new-product">
+          <Button variant="outlined" size="large" color="primary">
+            <img src={Star} alt="star icon" />
+          </Button>
+        </Link>
       </Toolbar>
     </ThemeProvider>
   );
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,84 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import ProductAppBAr from "../components/ProductAppBar";
+// material UI components
+import {
+  Table,
+  TableHead,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+} from "@mui/material";
+
+import DeleteIcon from "../assets/svg/delete-icon.svg";
+import EditIcon from "../assets/svg/edit-icon.svg";
+import StarIcon from "../assets/svg/star.svg";
+import StarredIcon from "../assets/svg/starred.svg";
 
 const Products = () => {
+  const [productList, setProductList] = useState([]);
+  const [liked, setLiked] = useState(false);
+
+  // get the data from the database when initial loading
+  useEffect(() => {
+    axios.get("http://localhost:4000/api/products").then((res) => {
+      setProductList(res.data);
+    });
+  }, []);
+
   return (
     <div>
       <ProductAppBAr />
-      All products
+      <TableContainer
+        sx={{
+          px: 3,
+        }}
+      >
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="left">SKU</TableCell>
+              <TableCell align="left">IMAGE</TableCell>
+              <TableCell align="left">PRODUCT NAME</TableCell>
+              <TableCell align="left">PRICE</TableCell>
+              <TableCell align="left"></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {productList.map((product) => {
+              const { _id, sku, name, quality, image } = product;
+              return (
+                <TableRow
+                  key={_id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  {/* <TableCell component="th" scope="row">
+                    {name}
+                  </TableCell> */}
+                  <TableCell align="left">{sku}</TableCell>
+                  <TableCell align="left">{image}</TableCell>
+                  <TableCell align="left">{name}</TableCell>
+                  <TableCell align="left">${quality}.00</TableCell>
+                  <TableCell align="right">
+                    <img src={DeleteIcon} alt="delete" />
+                    <img src={EditIcon} alt="delete" />
+                    <button
+                      onClick={() => setLiked(!liked)}
+                      style={{
+                        backgroundColor: "transparent",
+                        border: "none",
+                      }}
+                    >
+                      <img src={liked ? StarredIcon : StarIcon} alt="delete" />
+                    </button>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
